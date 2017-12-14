19d683531ad73f7087ec302e59997734dab0dd53
Michael Bender <mgb@grandorf.dk>
2017-12-14 14:56:57 +0100

    sandbox

---
+++ js/newhomepage.js
@@ -0,0 +1,68 @@
+window.$ = window.jQuery = require('jquery');
+
+
+$.fn.isOnScreen = function(){
+
+    var win = $(window);
+
+    var viewport = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+};
+
+$('body').scroll(function() {
+   if ($('#dark').isOnScreen() == true) {
+     $('.new').addClass('dark');   
+   }
+    else { 
+     $('.new').removeClass('dark');   
+   }
+});
+
+$('body').scroll(function() {
+   if ($('.data').isOnScreen() == true) {
+     $('.data').addClass('is-visible');   
+   }
+    else { 
+     $('.data').removeClass('is-visible');   
+   }
+});
+
+$('body').scroll(function() {
+   if ($('.iphonecontent').isOnScreen() == true) {
+     $('.iphonecontent').addClass('is-visible');   
+   }
+    else { 
+     $('.iphonecontent').removeClass('is-visible');   
+   }
+});
+
+
+$('body').scroll(function() {
+   if ($('.iphoneheader').isOnScreen() == true) {
+     $('.iphoneheader').addClass('is-visible');   
+   }
+    else { 
+     $('.iphoneheader').removeClass('is-visible');   
+   }
+});
+
+
+$('body').scroll(function() {
+   if ($('.shortcuts').isOnScreen() == true) {
+     $('.shortcuts').addClass('is-visible');   
+   }
+    else { 
+     $('.shortcuts').removeClass('is-visible');   
+   }
+});
